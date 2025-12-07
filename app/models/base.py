76b7c9b3b8356e3a6f89cd8d266d0dd0b65fb683
Ольга Пырьянова <pyryanova@mail.ru>
2025-12-07 19:17:58 +0300

    «3.0»

--- app/models/base.py
+++ app/models/base.py
@@ -1,29 +1,17 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Boolean, DateTime, Integer
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Boolean, Column, DateTime, Integer
 
-
-class Base(DeclarativeBase):
-    pass
+from app.core.db import Base
 
 
 class InvestableMixin:
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    full_amount: Mapped[int] = mapped_column(Integer, nullable=False)
-    invested_amount: Mapped[int] = mapped_column(
-        Integer, default=0, nullable=False
-    )
-    fully_invested: Mapped[bool] = mapped_column(
-        Boolean, default=False, nullable=False
-    )
-    create_date: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.utcnow, nullable=False
-    )
-    close_date: Mapped[Optional[datetime]] = mapped_column(
-        DateTime,
-        nullable=True
-    )
+    id = Column(Integer, primary_key=True)
+    full_amount = Column(Integer, nullable=False)
+    invested_amount = Column(Integer, default=0, nullable=False)
+    fully_invested = Column(Boolean, default=False, nullable=False)
+    create_date = Column(DateTime, default=datetime.utcnow, nullable=False)
+    close_date = Column(DateTime, nullable=True)
--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,17 @@
-from typing import List
-
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import relationship
 
-from app.models.donation import Donation
 from app.core.db import Base
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = 'user'
 
-    __allow_unmapped_annotations__ = True
+    id = Column(Integer, primary_key=True)
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    donations: Mapped[List['Donation']] = relationship(
-        back_populates='user', cascade='all, delete-orphan'
+    donations = relationship(
+        'Donation',
+        back_populates='user',
+        cascade='all, delete-orphan',
     )
