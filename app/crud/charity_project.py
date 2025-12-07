0bd2fa378a74507a7b31a08ed0a3063a6d426990
Ольга Пырьянова <pyryanova@mail.ru>
2025-12-07 15:02:47 +0300

    «3.0»

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -26,11 +26,11 @@ class CRUDCharityProject(CRUDBase[CharityProject, CharityProjectCreate]):
     ) -> list[CharityProject]:
         duration = (
             (extract('year', CharityProject.close_date) -
-             extract('year', CharityProject.create_date)) * DAYS_IN_YEAR
-            + (extract('month', CharityProject.close_date) -
-               extract('month', CharityProject.create_date)) * DAYS_IN_MONTH
-            + (extract('day', CharityProject.close_date) -
-               extract('day', CharityProject.create_date))
+             extract('year', CharityProject.create_date)) * DAYS_IN_YEAR +
+            (extract('month', CharityProject.close_date) -
+             extract('month', CharityProject.create_date)) * DAYS_IN_MONTH +
+            (extract('day', CharityProject.close_date) -
+             extract('day', CharityProject.create_date))
         )
         result = await session.execute(
             select(CharityProject).where(
